f51f5c3320336f3d50ca19789cbc0b743da870b2
Christian Zuckero <christianazuckero@gmail.com>
2024-03-28 01:12:32 -0400

    Rendered puppies from array in DOM

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
-import './App.css'
+import { useState } from 'react';
+import './App.css';
+import {puppyList} from './data.js';
+
 
 function App() {
-
+console.log("puppyList:", puppyList);
+const [puppies, setPuppies] = useState(puppyList)
   return (
-    <>
-
-    </>
+    <div className='App'>
+    {
+      puppies.map((puppy) => {
+        return <p key={puppy.id}>{puppy.name}</p>
+      })
+    }
+    </div>
   )
 }
 
